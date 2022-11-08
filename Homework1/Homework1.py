31809a249229b290c5db3a7ff1f822fcbbe27a0f
maxim <maxim@LAMO>
2022-11-08 20:04:17 +0200

    adding ex 1,2,4

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -1,19 +1,41 @@
 ##Homework1 
 ## Shay Hahiashvili , id: 206423840
-## Maxim Subotin , id: ?
+## Maxim Subotin , id: 207695479
 
 #Task1
 
+def even_str(word):
+    '''even_str gets a string, and returns the all the letters that are in the EVEN position, returns a string.'''
+    return word[1::2] # 1 stands for starting on the second letter, and 2 stands for jumping 2 indexes.
 
 
-
-
-print("hello")
+#print(even_str('Python')) # checking that the function even_str works.
 
 
 
 #Task2
 
+def pattern(n):
+    '''prints a pattern of stars with a gap in the middle. n represents the number of rows.'''
+    # variable decleration
+    i = 1
+    word = '*'
+
+    while i <= n: # outside loop is for printing rows.
+        j = i*2 # j is the number of stars in row number i
+        k = (n*2)-j # k is the number of spaces in row number i
+        while j > 0: # first inner loop is for printing stars in each row.
+            if j == i: # if we have allready printed exactly half of the stars for this row, go ahead and print all the spaces.
+                while k > 0: # second inner loop is for printing all the spaces at ones in each row.
+                    print(' ',end='')
+                    k-=1
+            print(word,end='')
+            j-=1
+        print()
+        i+=1
+        
+
+#pattern(5) # testing that the pattern function works
 
 
 
@@ -38,7 +60,19 @@ def sort_num(x):
 
 #Task4
 
+def min_dig(n):
+    '''min_dig gets a number and returns the lowest digit in that number.'''
+    if n<10: # if the number is a single digit, return it as it is the lowest.
+        return n
+    else: # else, keep units digit in the variable m and the next digit in k.
+        m = min_dig(n//10)
+        k = n % 10
+        if m < k: # then compare the two digits. returns m if its the min digit, other wise returns the current min digit which is k.
+           return m 
+        return k
 
+
+#print(min_dig(93163)) # testing that the pattern function works
 
 
 
@@ -73,3 +107,39 @@ def Xnor(x,y):
 
 
 print(Xnor(True,False)) #checks Xnor func
+
+
+
+
+
+
+
+
+
+def pattern(n):
+    '''prints a pattern of stars with a gap in the middle. n represents the number of rows.'''
+    # variable decleration
+    i = 1
+    word = '*'
+
+    while i <= n: # outside loop is for printing rows.
+        j = i*2 # j is the number of stars in row number i
+        k = (n*2)-j # k is the number of spaces in row number i
+        while j > 0: # first inner loop is for printing stars in each row.
+            if j == i: # if we have allready printed exactly half of the stars for this row, go ahead and print all the spaces.
+                while k > 0: # second inner loop is for printing all the spaces at ones in each row.
+                    print(' ',end='')
+                    k-=1
+            print(word,end='')
+            j-=1
+        print()
+        i+=1
+        
+
+#pattern(5) # testing that the pattern function works
+
+
+
+
+
+
